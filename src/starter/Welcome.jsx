85eccd2e69a68d6f8bf364e064961ba2e700927f
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-19 13:00:28 +0500

    welcome

--- src/starter/Welcome.jsx
+++ src/starter/Welcome.jsx
@@ -3,20 +3,20 @@ import logo from '../assets/g14.png'
 
 const Welcome = () => {
     return (
-        <div className="max-w-[400px] flex flex-col gap-y-4 px-5">
+        <div className=" flex flex-col items-center text-center pt-10 justify-around h-[100dvh]">
             <div className="flex items-center gap-x-2">
                 <img src={logo} alt="img" className="w-14"/>
                 <h1 className="text-4xl font-bold">Tamang <br /> FoodService</h1>
             </div>
-            <div>
+            <div className="">
                 <img src={illustration} alt="illustration" />
             </div>
             <div>
                 <h3 className="text-2xl font-semibold">Welcome</h3>
-                <p className="text-base">It’s a pleasure to meet you. We are excited that you’re here so let’s get started!</p>
+                <p className="text-base px-5">It’s a pleasure to meet you. We are excited that you’re here so let’s get started!</p>
             </div>
-            <footer>
-                <button>GET STARTER</button>
+            <footer className="w-full">
+                <button className="bg-primary-50 text-white text-base w-full py-2 rounded-md">GET STARTER</button>
             </footer>
         </div>
     )
