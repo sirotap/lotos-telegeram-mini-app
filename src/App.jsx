8daf778a5b5ebd8fd75671fb464845aba311be00
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-19 15:33:13 +0500

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,36 @@ import { Toaster } from 'react-hot-toast';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import Start from './starter/Welcome';
+import LoadingPage from './components/Loading';
+
 export default function App() {
     const [isLoading, setIsLoading] = useState(true);
-    // const location = useLocation();
+    const [loadingClass, setLoadingClass] = useState('loading-anima');
 
     useEffect(() => {
         Aos.init({ duration: 800 });
 
-        // Simulate loading time
-        const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 3000); // Adjust the time as needed
-
-        return () => clearTimeout(timer);
+        // Sahifa to'liq yuklanganini kutish
+        window.addEventListener('load', () => {
+            setLoadingClass('loading-anima slide-up'); // Animatsiyani boshlash
+            setTimeout(() => setIsLoading(false), 500); // Animatsiya tugagach, yuklanishni to'xtatish
+        });
 
     }, []);
 
     const routes = useRoutes([
         { path: '/', element: <Start /> },
         { path: '/welcome/*', element: <Start /> },
-
     ]);
-    // const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
 
     if (isLoading) {
-        return <h1>Yuklanmoqda</h1>;
+        return <div className={loadingClass}><LoadingPage /></div>;
     }
+
     return (
         <>
-            {/* {!isAuthPage && <Index />} */}
-
             <Toaster position="top-right" reverseOrder={false} />
             {routes}
-            {/* {!isAuthPage && <Bottom />} */}
         </>
-
-    )
+    );
 }
