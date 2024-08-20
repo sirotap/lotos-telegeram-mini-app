ca072d6a84d976eb38c60f09b93c6389c5a34b46
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-20 19:18:19 +0500

    navbar

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -1,4 +1,4 @@
-import { CiForkAndKnife, CiSquareChevDown, CiSearch } from "react-icons/ci";
+import { CiForkAndKnife, CiSquareChevDown } from "react-icons/ci";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const NavbarBottom = () => {
@@ -11,21 +11,14 @@ const NavbarBottom = () => {
         <footer className="flex sticky bottom-0 bg-white">
             <div 
                 onClick={() => navigate('/dashboard')} 
-                className={`w-1/3 text-center text-sm font-semibold border p-2 flex flex-col items-center ${isActive('/dashboard') ? 'text-primary-50' : 'text-gray-400'}`}
+                className={`w-1/2 text-center text-sm font-semibold border p-2 flex flex-col items-center ${isActive('/dashboard') ? 'text-primary-50' : 'text-gray-400'}`}
             >
                 <CiForkAndKnife className="text-2xl" />
                 <h2 className="mt-1 font-normal">Home</h2>
             </div>
             <div 
-                onClick={() => navigate('search')} 
-                className={`w-1/3 text-center text-sm font-semibold border p-2 flex flex-col items-center ${isActive('/dashboard/search') ? 'text-primary-50' : 'text-gray-400'}`}
-            >
-                <CiSearch className="text-2xl" />
-                <h2 className="mt-1 font-normal">Qidirish</h2>
-            </div>
-            <div 
                 onClick={() => navigate('orders')} 
-                className={`w-1/3 text-center text-sm font-semibold border p-2 flex flex-col items-center ${isActive('/dashboard/orders') ? 'text-primary-50' : 'text-gray-400'}`}
+                className={`w-1/2 text-center text-sm font-semibold border p-2 flex flex-col items-center ${isActive('/dashboard/orders') ? 'text-primary-50' : 'text-gray-400'}`}
             >
                 <CiSquareChevDown className="text-2xl" />
                 <h2 className="mt-1 font-normal">Orders</h2>
